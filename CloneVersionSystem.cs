8e2b506ff689c58b64a0e63237c35a912f9ec1d9
littllittl <assa19.98@mail.ru>
2019-03-07 17:12:45 +0300

    Добавьте файлы проекта.

---
+++ CloneVersionSystem.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+
+namespace Clones
+{
+    public class CloneVersionSystem : ICloneVersionSystem
+    {
+        private readonly List<Clone> clones = new List<Clone> { new Clone() };
+
+        public string Execute(string query)
+        {
+            var queries = query.Split();
+            var id = int.Parse(queries[1]) - 1;
+            var clone = clones[id];
+            switch (queries[0])
+            {
+                case "learn":
+                    clone.Learn(int.Parse(queries[2]));
+                    return null;
+                case "rollback":
+                    clone.Rollback();
+                    return null;
+                case "relearn":
+                    clone.Relearn();
+                    return null;
+                case "check":
+                    return clone.Check();
+                case "clone":
+                    clones.Add(clone.GetClone());
+                    return null;
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class Clone
+    {
+        private Stack<int> learned;
+        private Stack<int> cancelled;
+
+        public Clone()
+        {
+            learned = new Stack<int>();
+            cancelled = new Stack<int>();
+        }
+
+        public Clone(Stack<int> learned, Stack<int> cancelled)
+        {
+            this.learned = learned;
+            this.cancelled = cancelled;
+        }
+
+        public void Learn(int program)
+        {
+            if (!learned.Contains(program))
+            {
+                cancelled.Clear();
+                learned.Push(program);
+            }
+        }
+
+        public void Rollback()
+        {
+            if (learned.Count != 0)
+                cancelled.Push(learned.Pop());
+        }
+
+        public void Relearn()
+        {
+            if (cancelled.Count != 0)
+                learned.Push(cancelled.Pop());
+        }
+
+        public Clone GetClone() =>
+            new Clone(learned.Clone(), cancelled.Clone());
+
+        public string Check() =>
+                learned.Count == 0 ? "basic" : learned.Peek().ToString();
+    }
+
+    public class Stack<T>
+    {
+        private StackNode last;
+        public int Count { get; private set; }
+
+        public T Peek()
+        {
+            return last.Value;
+        }
+
+        public T Pop()
+        {
+            var value = last.Value;
+            last = last.Previous;
+            Count--;
+            return value;
+        }
+
+        public void Push(T element)
+        {
+            var next = new StackNode { Value = element };
+            Count++;
+            if (last != null) next.Previous = last;
+            last = next;
+        }
+
+        public Stack<T> Clone()
+        {
+            return new Stack<T> { last = last, Count = Count };
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            last = null;
+        }
+
+        public bool Contains(T data)
+        {
+            var current = last;
+            while (current != null)
+            {
+                if (current.Value.Equals(data))
+                    return true;
+                current = current.Previous;
+            }
+            return false;
+        }
+
+        private class StackNode
+        {
+            public StackNode Previous;
+            public T Value;
+        }
+    }
+}
---
+++ CloneVersionSystem_should.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Clones
+{
+	[TestFixture]
+	public class CloneVersionSystem_should
+	{
+		[Test]
+		public void Learn()
+		{
+			var clone1 = Execute("learn 1 45", "learn 1 46", "rollback 1", "check 1").Single();
+			Assert.AreEqual("45", clone1);
+		}
+
+		[Test]
+		public void RollbackToBasic()
+		{
+			var clone1 = Execute("learn 1 45", "rollback 1", "check 1").Single();
+			Assert.AreEqual("basic", clone1);
+		}
+
+		[Test]
+		public void RollbackToPreviousProgram()
+		{
+			var clone1 = Execute("learn 1 45", "learn 1 100500", "rollback 1", "check 1").Single();
+			Assert.AreEqual("45", clone1);
+		}
+
+		[Test]
+		public void RelearnAfterRollback()
+		{
+			var clone1 = Execute("learn 1 45", "rollback 1", "relearn 1", "check 1").Single();
+			Assert.AreEqual("45", clone1);
+		}
+
+		[Test]
+		public void CloneBasic()
+		{
+			var clone2 = Execute("clone 1", "check 2").Single();
+			Assert.AreEqual("basic", clone2);
+		}
+
+		[Test]
+		public void CloneLearned()
+		{
+            List<string> clone2 = Execute("learn 1 42", "clone 1", "clone 2", "clone 3","check 1", "check 2", "check 3", "check 4");
+			Assert.AreEqual(new[] { "42", "42","42","42" }, clone2);
+
+		}
+
+		[Test]
+		public void LearnClone_DontChangeOriginal()
+		{
+			List<string> res = Execute("learn 1 42", "clone 1", "learn 2 100500", "check 1", "check 2");
+			Assert.AreEqual(new []{ "42", "100500"}, res);
+		}
+
+		[Test]
+		public void RollbackClone_DontChangeOriginal()
+		{
+			var res = Execute("learn 1 42", "clone 1", "rollback 2", "check 1", "check 2");
+			Assert.AreEqual(new[] { "42", "basic" }, res);
+		}
+
+		[Test]
+		public void ExecuteSample()
+		{
+			var res = Execute(
+                "learn 1 8",
+                "check 1",//8
+                "learn 1 7",
+                "check 1",//7
+        
+                "clone 1",
+                "rollback 2",
+        "check 2",//8
+                "relearn 2",
+                "check 2",//7
+                "rollback 2",
+                "check 2",//8
+                "clone 2",
+                "relearn 3",
+                "check 3",//7
+                "rollback 3",
+                "check 3"//8
+                );
+			Assert.AreEqual(new[] { "8","7","8", "7","8","7","8" }, res);
+		}
+
+		private List<string> Execute(params string[] queries)
+		{
+			var cvs = Factory.CreateCVS();
+			var results = new List<string>();
+			foreach (var command in queries)
+			{
+				var result = cvs.Execute(command);
+				if (result != null) results.Add(result);
+			}
+			return results;
+		}
+	}
+}
